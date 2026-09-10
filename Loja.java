e148a7235f8df9d83ba48a09ff0718a16fa35015
André Santos <andresantos241@hotmail.com>
2026-09-10 15:55:04 -0300

    Add files via upload

---
+++ Loja.java
@@ -0,0 +1,29 @@
+public class Loja {
+    private String Nome;
+    private float Preco;
+    private int Quantidade;
+    
+    public Loja(String Nome, float Preco, int Quantidade){
+        this.Nome=Nome;
+        this.Preco=Preco;
+        this.Quantidade=Quantidade;
+    }
+
+    public String getNome(){
+        return Nome;
+    }
+
+    public void setNome(String Nome){
+        this.Nome=Nome;
+    }
+
+    public float getPreco(){
+        return Preco;
+    }
+
+    public void setPreco(float Preco){
+        this.Preco=Preco
+    }
+
+    public int getQuantidade()
+}
